787adba8e4353a179ea34bb6ac1e22242d614e2d
Vitor Galeti <vitor.fgaletti@gmail.com>
2024-12-02 23:13:26 -0300

    feat: Builder

---
+++ Builder/Builder/Program.cs
@@ -0,0 +1,39 @@
+﻿using Builder;
+
+HamburguerBuilder hamburguerBuilder = new();
+
+// X-Salada
+var xSalada = hamburguerBuilder.SetTipoPao("Branco")
+                               .SetTipoCarne("Bovina")
+                               .TemSalada()
+                               .Build();
+
+
+
+// X-Bacon
+hamburguerBuilder = new();
+var xBacon = hamburguerBuilder.SetTipoPao("Branco")
+                              .SetTipoCarne("Bovina")
+                              .TemSalada()
+                              .TemBacon()
+                              .TemQueijo()
+                              .Build();
+                               
+        
+
+// X-Tudo
+hamburguerBuilder = new();
+var xTudo = hamburguerBuilder.SetTipoPao("Branco")
+                             .SetTipoCarne("Bovina")
+                             .TemBacon()
+                             .TemQueijo()
+                             .TemSalada()
+                             .TemBatataPalha()
+                             .Build();
+
+
+Console.WriteLine(xSalada.GetDescricao());  
+Console.WriteLine("========================================");  
+Console.WriteLine(xBacon.GetDescricao());   
+Console.WriteLine("========================================"); 
+Console.WriteLine(xTudo.GetDescricao());                       
---
+++ Builder/README.md
@@ -0,0 +1,17 @@
+# Builder
+
+Separar a construção de um objeto completo de sua representação de modo que o mesmo processo de
+construção possa criar diferentes representações.
+
+## Características principais do Builder Pattern:
+- Separação da Construção da Representação: O processo de construção do objeto é separado da sua representação final.
+- Flexibilidade: Permite criar diferentes representações de um objeto com o mesmo código de construção.
+- Encapsulamento: A lógica de construção de um objeto é encapsulada dentro de uma classe específica (o "Builder").
+- Imutabilidade: Frequentemente, o objeto final gerado por um builder é imutável, ou seja, após sua construção, ele não pode ser modificado.
+
+
+## Quando usar:
+- Quando você precisa criar objetos complexos com diferentes combinações de características.
+- Quando a criação de um objeto requer uma série de etapas ou quando a ordem das etapas é importante.
+- Quando você deseja manter o código cliente desacoplado da construção do objeto.
+
